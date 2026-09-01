5142582d0ef7d1c6fc4c29f2a1da74ceb2e2ecb2
Christopher Haucke <132518562+CHaucke89@users.noreply.github.com>
2026-09-01 07:14:09 -0400

    Auto-auto-regen

--- opendbc/car/hyundai/hyundaicanfd.py
+++ opendbc/car/hyundai/hyundaicanfd.py
@@ -86,11 +86,12 @@ def create_suppress_lfa(packer, CAN, lfa_block_msg, lka_steering_alt):
   return packer.make_can_msg(suppress_msg, CAN.ACAN, values)
 
 
-def create_buttons(packer, CP, CAN, cnt, btn):
+def create_buttons(packer, CP, CAN, cnt, btn, right_paddle=False):
   values = {
     "COUNTER": cnt,
     "SET_ME_1": 1,
     "CRUISE_BUTTONS": btn,
+    "RIGHT_PADDLE": right_paddle,
   }
 
   bus = CAN.ECAN if CP.flags & HyundaiFlags.CANFD_LKA_STEER_MSG else CAN.CAM
